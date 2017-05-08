24446ea9a0a886152fff900acca794dea9cf27f2
Caio César Gallo <caio.gallo@gmail.com>
2017-05-08 15:31:58 -0300

    correção porta do postgres e adicionador senha ao usuario do banco

--- docker/postgres/init-user-db.sh
+++ docker/postgres/init-user-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER gateway;
+    CREATE USER gateway WITH PASSWORD '123456';
     CREATE DATABASE gatewaydb;
     GRANT ALL PRIVILEGES ON DATABASE gatewaydb TO gateway;
 EOSQL
