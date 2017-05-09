4d8194cff51399d268272a0cf038be8a0b59ee42
Cássio Sousa <cassio.sousa@accurate.com.br>
2017-05-09 15:59:00 -0300

    Inclusão de definição e execução do aplicativo gateway-client-service

--- java/gateway-client-service/src/test/java/br/com/accesstage/GatewayClientServiceApplicationTests.java
+++ java/gateway-client-service/src/test/java/br/com/accesstage/GatewayClientServiceApplicationTests.java
@@ -2,12 +2,14 @@ package br.com.accesstage;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootConfiguration
 @SpringBootTest
-public class GatewayApiApplicationTests {
+public class GatewayClientServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
